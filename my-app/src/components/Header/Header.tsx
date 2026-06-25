16ca8cb134955f621e9c7cece8806e2814b173ec
aurelienlozes <aurelienlozes@gmail.com>
2026-06-25 18:43:36 +0200

    center body

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Header = () => {
     //console.log("Header component rendered");
     return (
-        <header className={styles.header}>
+        <header>
             <div className={styles.headerLogo}>
                 <Image src="/images/logo.svg" alt="Logo" width={207} height={51} />
             </div>
