c661b39c236f39446089163efcc145101d573016
aurelienlozes <aurelienlozes@gmail.com>
2026-06-25 18:35:42 +0200

    Update header

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </main>
           <Footer />
         </div>
-
       </body>
     </html>
   );
---
+++ my-app/src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from "@/app/layout.module.css";
+import Image from "next/image";
+
+const Header = () => {
+    //console.log("Header component rendered");
+    return (
+        <header className={styles.header}>
+            <div className={styles.headerLogo}>
+                <Image src="/images/logo.svg" alt="Logo" width={207} height={51} />
+            </div>
+            <div className={styles.headerBackground}>
+                <Image  src="/images/banner.jpg" alt="Banner" objectFit="cover" objectPosition="center" fill  />
+            </div>
+        </header>
+    );
+}
+
+export default Header;
